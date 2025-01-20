f5fc3664ea9862e902abfbe1834133b9d3eaa111
Teachers <Teachers@LAB52-PC11>
2025-01-20 19:38:54 +0200

    Add project files.

---
+++ ProductAPI/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace ProductAPI.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Phone { get; set; }
+    }
+}
+
+
---
+++ ProductAPI/ProductsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+
+namespace ProductAPI
+{
+
+    public class ProductsDbContext:DbContext
+    {
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
